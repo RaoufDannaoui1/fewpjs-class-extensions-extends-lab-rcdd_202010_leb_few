80ab16d1afbec840818e8d978cf93109c3470b84
AbdelRaouf Dannaoui <123raoufdannaoui@gmail.com>
2020-12-15 19:26:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Your code here
+class Polygon {
+  constructor(array) {
+    this.array=array;
+  }
+
+  get countSides(){
+    return this.array.length;
+  }
+
+  get perimeter(){
+    for (var int of this.sides) {
+      sum += int
+    }
+    return sum
+  }
+}
